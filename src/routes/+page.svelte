0311bd06da9eb1407a869e57ade506280b070a15
fiq <me@taufiqhdyt.com>
2023-08-23 12:54:33 +0700

    feat: create emoji list, init base state & logic
    - create grid based on half size
    - create shuffle function that short all item in grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { emoji } from '$lib/emoji'
+
+  type State = 'init' | 'playing' | 'paused' | 'won' | 'lost'
+
+  let state: State = 'init'
+  let size: number = 20
+  let grid = createGrid()
+
+  function createGrid() {
+    let cards = new Set<string>()
+    let maxSize = size / 2
+
+    while (cards.size < maxSize) {
+      const randomIndex = Math.floor(Math.random() * emoji.length)
+      cards.add(emoji[randomIndex])
+    }
+
+    return shuffle([...cards, ...cards])
+  }
+
+  function shuffle<Items>(array: Items[]) {
+    return array.sort(() => Math.random() - 0.5)
+  }
+
+  console.log(grid)
+</script>
